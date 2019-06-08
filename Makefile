923fe257c2db432c3271c6d84f6dcabc889716ed
Timm Knape <timm@knp.de>
2019-06-08 18:32:34 +0200

    running

--- Makefile
+++ Makefile
@@ -3,10 +3,12 @@
 TARGET := riscv32-unknown-elf
 CC := $(TARGET)-gcc
 LD := $(TARGET)-ld -T memory.lds
+AS := $(TARGET)-as
+CFLAGS := -O2
 
 all: hello.hex
 
-hello: hello.o
+hello: hello.o entry.o
 	$(LD) $^ -o $@
 	
 hello.hex: hello
--- hello.c
+++ hello.c
@@ -1,17 +1,36 @@
+static volatile int *port = (int *) 268513280ul;
+
+static inline void enable_out() {
+	port[0x08] |= 1;
+	port[0x0c] |= 1;
+}
+
 static inline void my_putc(char c) {
-	static volatile int *port = (int *) 268513280ul;
 	while ((*port & (1 << 31)) != 0) { }
 	    *port = c;
 }
 
-static inline void my_puts(const char *c) {
-	for (; *c; ++c) {
-		my_putc(*c);
-	}
+static inline void my_msg() {
+	my_putc('H');
+	my_putc('e');
+	my_putc('l');
+	my_putc('l');
+	my_putc('o');
+	my_putc(' ');
+	my_putc('W');
+	my_putc('o');
+	my_putc('r');
+	my_putc('l');
+	my_putc('d');
+	my_putc('!');
+	my_putc('\r');
 	my_putc('\n');
 }
 
-void _enter() {
-	my_puts("Hello, World!");
+static const char msg[] = "Hello World!";
+
+void _start() {
+	enable_out();
+	my_msg();
 	for (;;) {}
 }
